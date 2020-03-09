abea9da3404719392d148b4e1e14bf52e1d31632
zengmiaokun <zengmiaokun@live.com>
2020-03-09 20:49:02 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,4 +6,7 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+clone https://github.com/jefferymvp/luci-app-koolproxyR package/luci-app-koolproxyR
+./scripts/feeds update -a
+./scripts/feeds install -a
