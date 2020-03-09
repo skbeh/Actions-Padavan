f4b485d59e0278bf9578ef98e80f5bec1f9f3d3a
zengmiaokun <zengmiaokun@live.com>
2020-03-09 20:58:08 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,6 +7,6 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
-clone https://github.com/jefferymvp/luci-app-koolproxyR package/luci-app-koolproxyR
+git clone https://github.com/jefferymvp/luci-app-koolproxyR package/luci-app-koolproxyR
 ./scripts/feeds update -a
 ./scripts/feeds install -a
